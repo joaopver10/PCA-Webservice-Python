90aced9936d518fdca4ad59add0830f5f2e97af8
João Pedro <55984774+joaopver10@users.noreply.github.com>
2021-10-12 18:27:58 -0300

    Adicionando migrations e Pasta para arquivos estaticos

--- ProjetoPCA/models.py
+++ ProjetoPCA/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Estados(models.Model):
+    sigla = models.CharField('Sigla', max_length=2)
+    descricao = models.TextField('Descrição')
+
+    def __str__(self):
+        return self.sigla
